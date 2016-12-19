98c11bc4b4bf7c08672b633182556293b8e8b06d
Tulio Macedo <tuliomac@gmail.com>
2016-12-19 19:29:18 -0200

    adições ao cadastro.rb
    inclusão de instruções para uso, inclusive início

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source "https://rubygems.org"
 gem 'cucumber', '2.4.0'
 gem 'capybara', '2.7.1'
 gem 'selenium-webdriver', '2.53.4'
+gem 'page-object', 'https://github.com/cheezy/page-object'
+gem 'watir-webdriver', 'http://watirwebdriver.com/'
+gem 'watir',:plataforms=>[:mswin, :mingw] - http://watir.com/
--- features/step_definitions/cadastro.rb
+++ features/step_definitions/cadastro.rb
@@ -1,12 +1,17 @@
 Dado(/^que eu estou na página de cadastro do Groupon$/) do
+  #@url = "http://sdn.tucano.net.br/wp-login.php"
+  #visit ('@url')
   visit "http://sdn.tucano.net.br/wp-login.php"
   #sleep 10
 end
 
 Quando(/^eu preencher os campos obrigatórios$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  #select 'Sr.', from: 'gender-select'
+  #find(:id, 'first-name-input').set("Nome")
+  #find(:css, '#first-name-input').set("Nome") por id
+  #find(:css, '.first-name-input').set("Nome") por classe
 end
 
 Então(/^o cadastro deverá ser realizado com sucesso$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  # Write code here that turns the phrase above into concrete actions
 end
