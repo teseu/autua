0e1222cab6b1522ed72e4bf1b641c4bb8512a04b
Tulio Macedo <tuliomac@gmail.com>
2017-01-13 16:16:09 -0200

    pequenos ajustes e inclusão do driver para FF

--- features/step_definitions/comentario.rb
+++ features/step_definitions/comentario.rb
@@ -4,17 +4,18 @@ $pass = lines[1]
 
 Dado(/^que estou na página de gestão de comentários$/) do
   @url_login = "http://sdn.tucano.net.br/wp-login.php"
+  Capybara.reset_sessions!
   visit (@url_login)
   fill_in('user_login', :with => $user)
-  #fill_in('user_pass', :with => $pass)
   find(:id, 'user_pass').set($pass)
-  find(:id, 'wp-submit').click
 end
 
 Quando(/^eu selecionar o comentário$/) do
   @url_comentario = 'http://sdn.tucano.net.br/wp-admin/edit-comments.php'
   visit (@url_comentario)
-  $num_comentarios_antes = page.first('span.pending-count').text
+  #$num_comentarios_antes = page.first('span.pending-count').text
+  $num_comentarios_antes = find(:xpath, '//*[@id="menu-comments"]/a/div[3]/span/span').text
+
   find(:xpath, '//*[@id="the-comment-list"]/tr[1]/th/input').click
 end
 
@@ -30,4 +31,5 @@ Então(/^o comentáio deverá ser eliminado$/) do
   puts $num_comentarios_antes
   puts $num_comentarios_depois
   expect($num_comentarios_antes).not_to eq($num_comentarios_depois)
+  Capybara.reset_sessions!
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,13 +5,20 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
+# Capybara.register_driver :selenium_firefox do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :firefox, marionette: true)
+# end
+
 Capybara.javascript_driver = :chrome
+#Capybara.javascript_driver = :selenium_firefox
 
 Selenium::WebDriver::Chrome.driver_path = 'features/support/chromedriver/chromedriver.exe'
 #Selenium::WebDriver::Chrome.driver_path = 'features/support/chromedriver/chromedriver'
+Selenium::WebDriver::Firefox.driver_path = 'features/support/geckodriver/geckodriver.exe'
 
 Capybara.default_driver = :chrome
-Capybara.default_max_wait_time = 5
+#Capybara.default_driver = :selenium_firefox
+Capybara.default_max_wait_time = 15
 
 #arquivo com dados de acesso
 $acesso = 'features/support/acesso'
